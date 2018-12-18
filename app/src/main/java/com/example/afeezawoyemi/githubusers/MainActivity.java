910e64eadda2a69f2658e14bf85fbe543496d1f6
damafeez <damafeez@gmail.com>
2018-12-18 11:17:15 +0100

    feature(recycler-view) connect recycler to MainActivity
    - connect RecyclerView to MainActivity
    - put recycler_view directly into activity_main
    
    finishes #15768857

--- app/src/main/java/com/example/afeezawoyemi/githubusers/MainActivity.java
+++ app/src/main/java/com/example/afeezawoyemi/githubusers/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.afeezawoyemi.githubusers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView users;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        users = findViewById(R.id.rv_users);
     }
 }
