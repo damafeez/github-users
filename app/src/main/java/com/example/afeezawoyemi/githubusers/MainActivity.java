bf49006322b1d451fb5d2b4c5cae8b295b432c54
damafeez <damafeez@gmail.com>
2018-12-19 09:53:39 +0100

    feature(detailActivity) launch from mainActivity
    - add onClick handler to button in activity_main.xml
    - add function launchDetailActivity to MainActivity.java
    - remove 'view' from detailActivity layout
    
    finishes #155820624

--- app/src/main/java/com/example/afeezawoyemi/githubusers/MainActivity.java
+++ app/src/main/java/com/example/afeezawoyemi/githubusers/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.afeezawoyemi.githubusers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         users = findViewById(R.id.rv_users);
+    }
+
+    public void launchDetailActivity(View view) {
+        Log.d("QWER", "HELLO HERE");
+        Intent showDetailActivity = new Intent(this, DetailActivity.class);
+        Log.d("QWER", "HELLO HERE");
+        startActivity(showDetailActivity);
+        Log.d("QWER", "HELLO HERE");
     }
 }
