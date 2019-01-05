255c32ac02d72a81ea5b89a013cdac3ddddf6962
damafeez <damafeez@gmail.com>
2019-01-05 10:06:47 +0100

    fix(log): remove log
    finishes #155820624

--- app/src/main/java/com/example/afeezawoyemi/githubusers/MainActivity.java
+++ app/src/main/java/com/example/afeezawoyemi/githubusers/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,10 +17,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void launchDetailActivity(View view) {
-        Log.d("QWER", "HELLO HERE");
         Intent showDetailActivity = new Intent(this, DetailActivity.class);
-        Log.d("QWER", "HELLO HERE");
         startActivity(showDetailActivity);
-        Log.d("QWER", "HELLO HERE");
     }
 }
