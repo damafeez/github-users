60e7e9e41e647dc1b7ec2ab83b497b0e7cbdf14e
damafeez <damafeez@gmail.com>
2019-01-10 10:29:53 +0100

    feature(ui/ux): revamp ui/ux
    - add list_item_grid.xml
    - use grid layout in adapter
    - add progress bar
    - implement swipe to refresh functionality
    
    finishes #156037248

--- app/src/main/java/com/example/afeezawoyemi/githubusers/MainActivity.java
+++ app/src/main/java/com/example/afeezawoyemi/githubusers/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.afeezawoyemi.githubusers;
 
 import android.graphics.Color;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -19,6 +21,7 @@ public class MainActivity extends AppCompatActivity implements GithubUsersView{
     private GithubPresenter presenter;
     private RecyclerView userListRecycler;
     private Toolbar appToolbar;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +30,30 @@ public class MainActivity extends AppCompatActivity implements GithubUsersView{
 
         appToolbar = findViewById(R.id.action_bar);
         appToolbar.setTitleTextColor(Color.GRAY);
+        swipeRefreshLayout = findViewById(R.id.swipe_refresh_layout);
         presenter = new GithubPresenter(this);
         userListRecycler = findViewById(R.id.rv_users);
         presenter.getUsers();
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                presenter.getUsers();
+            }
+        });
+        swipeRefreshLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                swipeRefreshLayout.setRefreshing(true);
+                presenter.getUsers();
+            }
+        });
     }
 
     @Override
     public void githubUsersReady(List<GithubUser> users) {
-        userListRecycler.setLayoutManager(new LinearLayoutManager(this));
+        userListRecycler.setLayoutManager(new GridLayoutManager(this, 2));
         GithubAdapter adapter = new GithubAdapter(users);
         userListRecycler.setAdapter(adapter);
+        swipeRefreshLayout.setRefreshing(false);
     }
 }
